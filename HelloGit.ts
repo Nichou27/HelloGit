0adbee90b98d22cf3ba11db6837aaf85e6943e40
Ignacio Gonzalez <nachogonzalez2727@gmail.com>
2025-04-30 11:59:09 -0300

    Update HelloGit.ts

--- HelloGit.ts
+++ HelloGit.ts
@@ -1,3 +1,3 @@
-console.log("Hello, Git! This is a test file to check if the git commit works correctly.")
+console.log("Hello, Git! This is a test file to check if the git commit works correctly. Also, this is a test to see if the push works correctly. Let's see if it works!");
 
 
